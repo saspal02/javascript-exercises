6dd0265325dd8aac80a2fc21f4f5cc2a25b50084
Saswat Pal <saswat2001@gmail.com>
2024-03-29 10:47:59 +0530

    Solve temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  const conversion = (farenheit - 32) * 5/9;
+  const roundedConversion = parseFloat(conversion.toFixed(1));
+  return roundedConversion;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const conversion = (celsius * 9/5) + 32;
+  const roundedConversion = parseFloat(conversion.toFixed(1)); 
+  return roundedConversion;
 };
 
 // Do not edit below this line
